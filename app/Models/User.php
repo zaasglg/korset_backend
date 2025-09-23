cf0b2831399be039c03ecaab64cf8a3baa6b4f02
zaasglg <n4msin@mail.ru>
2025-09-23 10:18:05 +0500

    efewf

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,7 +52,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            // 'password' => 'hashed',
             'is_active' => 'boolean',
             'balance' => 'decimal:2',
         ];
